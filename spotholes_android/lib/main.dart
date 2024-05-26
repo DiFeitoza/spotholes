a86e29e831393b027bc777c7963c38eca76dd41d
Diego Feitoza <19846327+DiFeitoza@users.noreply.github.com>
2024-05-25 23:07:34 -0300

    feat: implements new app splash screen and icon

--- spotholes_android/lib/main.dart
+++ spotholes_android/lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:spotholes_android/config/environment_config.dart';
 import 'package:spotholes_android/pages/base_map_page.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(const Duration(seconds: 3));
   await EnvironmentConfig.loadEnvVariables();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
+  // whenever your initialization is completed, remove the splash screen:
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
